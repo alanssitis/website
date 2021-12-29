ec36ce8aaff5395e22a5e30ecc59aeec04b73e2d
Alan Chung Ma <alanchunggt@gmail.com>
2021-12-29 06:30:24 -0500

    initialize skeleton of project

---
+++ svelte-frontend/src/lib/components/NavBar.svelte
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<nav>
+    <a href="/">Home</a>
+    <a href="/blog">Blog</a>
+    <a href="/contact">Contact</a>
+</nav>
---
+++ svelte-frontend/src/routes/__error.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+    export function load({error, status}) {
+        return {
+            props: {
+                message: error.message,
+                errStatus: status
+            }
+        }
+    }
+</script>
+
+<script>
+    export let message = "error";
+    export let errStatus = "???";
+</script>
+
+<h1>{message}</h1>
+<h1>{errStatus}</h1>
