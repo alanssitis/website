907ce1ee790bb8feab790fe4be11bdffa0b8e05a
alanssitis <alanchunggt@gmail.com>
2022-11-25 17:38:52 -0500

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const SITE_TITLE = "Alan Chung Ma";
+export const SITE_DESCRIPTION = "This website is still WIP.";
+export const BLOG_TITLE = "BLOG TITLE";
+export const BLOG_DESCRIPTION = "blog descript";
+export const PROJECTS_TITLE = "PROJECTS TITLE";
+export const PROJECTS_DESCRIPTION = "projects descript";
