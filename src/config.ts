d8d0a7b6338b92c7da8949932bef25a1cdb748f3
alanssitis <alanchunggt@gmail.com>
2022-12-10 14:08:46 -0500

    improve footer alignment on all pages and titles

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const SITE_TITLE = "Alan Chung Ma";
 export const SITE_DESCRIPTION = "This website is still WIP.";
-export const BLOG_TITLE = "BLOG TITLE";
+export const BLOG_TITLE = "The Boring Blog";
 export const BLOG_DESCRIPTION = "blog descript";
-export const PROJECTS_TITLE = "PROJECTS TITLE";
+export const PROJECTS_TITLE = "ACM - Projects";
 export const PROJECTS_DESCRIPTION = "projects descript";
