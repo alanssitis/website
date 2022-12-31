68fa2714a93ffde5bea7a1376f27059f7c1e3c4c
alanssitis <alanchunggt@gmail.com>
2022-12-31 15:37:04 -0500

    add blog entry formatting

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const SITE_TITLE = "Alan Chung Ma";
 export const SITE_DESCRIPTION = "This website is still WIP.";
-export const BLOG_TITLE = "The Boring Blog";
-export const BLOG_DESCRIPTION = "blog descript";
+export const BLOG_TITLE = "Blog";
+export const BLOG_DESCRIPTION = "Alan Chung's blog";
 export const PROJECTS_TITLE = "ACM - Projects";
 export const PROJECTS_DESCRIPTION = "projects descript";
---
+++ src/pages/blog/first.md
@@ -0,0 +1,10 @@
+---
+layout: "../../layouts/BlogPostLayout.astro"
+title: "First Post"
+description: "A test post"
+pubDate: "12/31/2022"
+---
+
+# First Post
+
+This is a test post. Happy new year!
