6aafab8bc697ed8bf73cb4cf81b88da8725a14bc
Alan Chung Ma <alanchunggt@gmail.com>
2023-04-09 11:06:59 -0400

    prettify plugin and update project & index

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@ export const SITE_TITLE = "Alan Chung Ma";
 export const SITE_DESCRIPTION = "This website is still WIP.";
 export const BLOG_TITLE = "Blog";
 export const BLOG_DESCRIPTION = "Alan Chung's blog";
-export const PROJECTS_TITLE = "ACM - Projects";
+export const PROJECTS_TITLE = "Projects";
 export const PROJECTS_DESCRIPTION = "projects descript";
