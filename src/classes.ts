0e8ba84d196edb79bc1acbfb6ea5f85e1ac8fea7
Alan Chung Ma <alanchunggt@gmail.com>
2023-04-09 20:51:20 -0400

    updated home landing page

---
+++ src/classes.ts
@@ -0,0 +1,237 @@
+export const Semesters = [
+  {
+    sem: "Fall 2020",
+    classes: [
+      {
+        name: "CHM 11500",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=13&coid=129195",
+        description: "General Chemistry",
+      },
+      {
+        name: "ENGR 16100",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=13&coid=133109",
+        description: "Honors Engineering 1",
+      },
+      {
+        name: "HIST 15200",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=13&coid=128189",
+        description: "US History Since 1877",
+      },
+      {
+        name: "MA 16500",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=13&coid=129760",
+        description: "Calculus 1",
+      },
+    ],
+  },
+  {
+    sem: "Spring 2021",
+    classes: [
+      {
+        name: "CS 15900",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=13&coid=129472",
+        description: "C Programming",
+      },
+      {
+        name: "EDPS 31500",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=13&coid=126783",
+        description: "Interpersonal Skills",
+      },
+      {
+        name: "ENGR 16200",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=13&coid=133110",
+        description: "Honors Engineering 2",
+      },
+      {
+        name: "MA 16200",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=13&coid=129759",
+        description: "Calculus 2",
+      },
+      {
+        name: "MSE 23000",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=13&coid=127310",
+        description: "Structure & Properties of Materials",
+      },
+    ],
+  },
+  {
+    sem: "Summer 2021",
+    classes: [
+      {
+        name: "ECE 26400",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=13&coid=126856",
+        description: "Advanced C Programming",
+      },
+      {
+        name: "MA 26100",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=13&coid=129766",
+        description: "Calculus 3",
+      },
+    ],
+  },
+  {
+    sem: "Fall 2021",
+    classes: [
+      {
+        name: "ECE 20001",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=14&coid=142869",
+        description: "Circuits 1",
+      },
+      {
+        name: "ECE 20007",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=14&coid=142871",
+        description: "Circuits Lab 1",
+      },
+      {
+        name: "ECE 20875",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=14&coid=142924",
+        description: "Python for Data Science",
+      },
+      {
+        name: "ECE 29401",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=14&coid=142865",
+        description: "Sophomore Seminar",
+      },
+      {
+        name: "ECE 36800",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=14&coid=142815",
+        description: "Data Structures",
+      },
+      {
+        name: "MA 26200",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=14&coid=138898",
+        description: "Linear Algebra & Differential Equations",
+      },
+    ],
+  },
+  {
+    sem: "Spring 2022",
+    classes: [
+      {
+        name: "CHNS 20200",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=14&coid=137132",
+        description: "Chinese 4",
+      },
+      {
+        name: "ECE 20002",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=14&coid=142870",
+        description: "Circuits 2",
+      },
+      {
+        name: "ECE 27000",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=14&coid=136213",
+        description: "Digital System Design",
+      },
+      {
+        name: "ECE 40400",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=14&coid=136248",
+        description: "Computer Security",
+      },
+      {
+        name: "VIP 27920",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=14&coid=142963",
+        description: "Project rekor VIP",
+      },
+    ],
+  },
+  {
+    sem: "Fall 2022",
+    classes: [
+      {
+        name: "CS 314",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=15&coid=156589",
+        description: "Numerical Methods",
+      },
+      {
+        name: "ECE 30100",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=15&coid=154116",
+        description: "Signals and Systems",
+      },
+      {
+        name: "ECE 36200",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=15&coid=160696",
+        description: "Microprocessor Systems and Interfacing",
+      },
+      {
+        name: "ECE 39401",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=15&coid=160718",
+        description: "Junior Seminar",
+      },
+      {
+        name: "ECE 57300",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=15&coid=158761",
+        description: "Compilers and Translator Writing Systems",
+      },
+    ],
+  },
+  {
+    sem: "Spring 2023",
+    classes: [
+      {
+        name: "ECE 30200",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=15&coid=154117",
+        description: "Probabilistic Methods",
+      },
+      {
+        name: "ECE 36900",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=15&coid=154129",
+        description: "Discrete Mathematics",
+      },
+      {
+        name: "ECE 46900",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=15&coid=154166",
+        description: "OS Engineering",
+      },
+      {
+        name: "ECE 49595",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=15&coid=154173",
+        description: "Game Theory",
+      },
+      {
+        name: "ECE 49600",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=15&coid=154174",
+        description: "MITRE eCTF",
+      },
+      {
+        name: "PES 11600",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=15&coid=154695",
+        description: "Golf",
+      },
+      {
+        name: "PHYS 27200",
+        link: "https://catalog.purdue.edu/preview_course.php?catoid=15&coid=156900",
+        description: "Physics E&M",
+      },
+    ],
+  },
+  {
+    sem: "Fall 2023 [TENTATIVE]",
+    classes: [
+      {
+        name: "ECE 49401",
+        link: "https://engineering.purdue.edu/ECE/Academics/Undergraduates/UGO/CourseInfo/courseInfo?courseid=723&show=true&type=undergrad",
+        description: "Senior Seminar",
+      },
+      {
+        name: "ECE 47700",
+        link: "https://engineering.purdue.edu/ECE/Academics/Undergraduates/UGO/CourseInfo/courseInfo?courseid=397&show=true&type=undergrad",
+        description: "Senior Design",
+      },
+      {
+        name: "ECE 51220",
+        link: "https://engineering.purdue.edu/ECE/Academics/Undergraduates/UGO/CourseInfo/courseInfo?courseid=695&show=true&type=undergrad",
+        description: "Applied Algorithms",
+      },
+      {
+        name: "ECE 57000",
+        link: "https://engineering.purdue.edu/ECE/Academics/Undergraduates/UGO/CourseInfo/courseInfo?courseid=71&show=true&type=undergrad",
+        description: "Artificial Intelligence",
+      },
+      {
+        name: "ECE 69500",
+        link: "https://engineering.purdue.edu/ECE/Academics/Undergraduates/UGO/CourseInfo/courseInfo?courseid=805&show=true&type=grad",
+        description: "Holistic Software Security",
+      },
+    ],
+  },
+];
