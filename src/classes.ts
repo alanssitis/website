c9f892ded318e5587eb7a1b4d61d9565b515ddbd
alanssitis <alanchunggt@gmail.com>
2023-06-24 16:24:46 -0700

    Updated projects page
    Updated projects section to contain more stuff.

--- src/classes.ts
+++ src/classes.ts
@@ -1,4 +1,4 @@
-export const Semesters = [
+export const SemesterArray = [
   {
     sem: "Fall 2020",
     classes: [
---
+++ src/projects.ts
@@ -0,0 +1,60 @@
+export const ProjectArray = [
+  {
+    title: "Autonomous Kart Software Stack",
+    description:
+      "Built v3 of AMP's (Autonomous Motorsports Purdue) Autonomous Software \
+    Stack on ROS 2 Foxy.",
+    url: "https://github.com/Autonomous-Motorsports-Purdue/AMP_ASSV3/",
+  },
+  {
+    title: "in-toto ITE-4 Generic Artifact Resolver Implementation",
+    description:
+      "Helped implement ITE-4 (generic artifact URI support) to in-toto's \
+      Python implementation. Aditya Sirish and Lukas Pühringer helped finishing it.",
+    url: "https://github.com/in-toto/in-toto/pull/584",
+  },
+  {
+    title: "Purdue eCTF Efforts",
+    description:
+      "Ported the lightweight crypto library Ascon to the target device (TI \
+      TM4C123GH6PM) and implemented the PRNG.",
+    url: "https://ectf.mitre.org/wp-content/uploads/2023/04/2023_eCTF_Purdue_Poster.pdf",
+  },
+  {
+    title: "ITE-2 Demo",
+    description:
+      "Demo for the in-toto project shown in Open Source Summit NA 2023. It \
+    demonstrates a novel approach of combining RSTUF with in-toto links \
+    enhanced with ITE-2 TUF metadata to create a more robust software supply chain.",
+    url: "https://github.com/alanssitis/ite-2-demo",
+  },
+  {
+    title: "ITE-4 Demo",
+    description:
+      "Demo for the in-toto project shown in KubeCon NA 2022. It demonstrates \
+    an example implementation of ITE 4 and possible use cases.",
+    url: "https://github.com/in-toto/ite-4-demo",
+  },
+  {
+    title: "Micro C Compiler",
+    description:
+      "Compiler written in Rust written for the compilers course ECE 573.",
+    url: "https://github.com/alanssitis/course-compiler-project",
+  },
+  {
+    title: "achungma.me",
+    description: "Astro static website hosted on Vercel.",
+    url: "https://github.com/alanssitis/website",
+  },
+  {
+    title: "IoT Device Hacking",
+    description: "Briefly messed around with IoT device hacking.",
+    url: "https://github.com/TSELab/IoT-Device-Hacking",
+  },
+  {
+    title: "Purdue Rekor Website",
+    description:
+      "Worked on Purdue's rekor website as a VIP project in Spring 2022.",
+    url: "https://github.com/TSELab/rekor-website",
+  },
+];
