cbac8831dc23a9317c5360fefcf724a2a76699d5
Alan Chung Ma <alanchunggt@gmail.com>
2024-03-30 12:45:39 -0400

    Update and clean up website structures

--- src/content/config.ts
+++
@@ -1,4 +0,0 @@
-export const SITE_TITLE = "Alan Chung Ma";
-export const SITE_DESCRIPTION = "Home page for Alan Chung Ma.";
-export const PROJECTS_TITLE = "Projects";
-export const PROJECTS_DESCRIPTION = "Alan Chung Ma's notable projects.";
