41a169a303b54d2c10ba56718ad1334c2eb68734
Alan Chung Ma <alan@chung-ma.com>
2025-09-28 12:20:28 +0200

    Added spanish locale

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,16 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   site: "https://alan.chung-ma.com",
   integrations: [mdx(), sitemap()],
+  i18n: {
+    locales: ["en", "es" ],
+    defaultLocale: "en",
+    fallback: {
+      es: "en",
+    },
+    routing: {
+      fallbackType: "rewrite",
+    },
+  },
   vite: {
     server: {
       watch: {
