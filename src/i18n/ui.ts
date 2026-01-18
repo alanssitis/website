66caa98ff4e07ee7ba7b2cf1dd7b69f119e76d5d
Alan Chung Ma <alan@chung-ma.com>
2026-01-18 22:18:38 +0100

    created i18n utils for ui

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+    'nav.publications': 'Publications',
+    'nav.talks': 'Talks',
+    'nav.service': 'Service',
+    'nav.blog': 'Blog',
+  },
+  es: {
+    'nav.home': 'Hogar',
+    'nav.about': 'Acerca de',
+    'nav.publications': 'Publicaciones',
+    'nav.talks': 'Platicas',
+    'nav.service': 'Servicio',
+  },
+} as const;
